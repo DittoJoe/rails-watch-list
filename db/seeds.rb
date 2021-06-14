915ec2f1786f3703d32412eb8b24001f5e73dbb0
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-14 15:02:17 +0200

    fixed seed so new movies have photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-Movie.create(title: "Easy A", overview: "When her high school brands her the 'Scarlet Letter', she leans into it and finds a new identity", poster_url: "https://i5.walmartimages.com/asr/28e33fcc-3a3f-4f9e-8c80-79f24a872987_1.931821f683ea45f2fe6fb3c05f99cf27.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff")
-Movie.create(title: "Frozen", overview: "LET IT GOOOOO ❄️", poster_url: "https://images-na.ssl-images-amazon.com/images/I/714arK1ZtCL._AC_SY741_.jpg")
-Movie.create(title: "Frozen 2", overview: "INTO THE UNKNOOOOOOOOOOOOWN ❄️", poster_url: "https://m.media-amazon.com/images/M/MV5BMjA0YjYyZGMtN2U0Ni00YmY4LWJkZTItYTMyMjY3NGYyMTJkXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_UX182_CR0,0,182,268_AL_.jpg")
+Movie.create(title: "Easy A", overview: "When her high school brands her the 'Scarlet Letter', she leans into it and finds a new identity", poster_url: "https://www.themoviedb.org/t/p/original/A1c64dtFHYLzeGaia4MuDoR4lNE.jpg")
+Movie.create(title: "Frozen", overview: "LET IT GOOOOO ❄️", poster_url: "https://www.themoviedb.org/t/p/original/hAKhrHvzQDUHQP5zd5HFeqF2BCN.jpg")
+Movie.create(title: "Frozen 2", overview: "INTO THE UNKNOOOOOOOOOOOOWN ❄️", poster_url: "https://www.themoviedb.org/t/p/original/jnFCk7qGGWop2DgfnJXeKLZFuBq.jpg")
 
+puts "Done!"
